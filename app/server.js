90d8b53744215d8c0f4ec985da1dbd1d12a20617
Giancarlo Kite <103792674+giancarlo-k@users.noreply.github.com>
2025-01-02 17:20:54 -0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -5,9 +5,9 @@ import path from 'path';
 import session from 'express-session';
 import connectMongo from 'connect-mongo';
 import { fileURLToPath } from 'url';
-import {routes as authRoutes} from './auth/routes.js'
-import {routes as snippetRoutes} from './snippets/routes.js'
-import { isAuthenticated } from './auth/controller.js'
+import {routes as authRoutes} from './auth/routes.js';
+import {routes as snippetRoutes} from './snippets/routes.js';
+import { isAuthenticated } from './auth/controller.js';
 
 const app = express();
 const port = 3000;
@@ -17,18 +17,24 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const MongoDbStore = require('connect-mongo');
+// Use 'new' with MongoStore
+const MongoStore = connectMongo(session);
 
-app.use(session({
-  secret: 'idkwhatthisdoesngl', 
-  resave: false,
-  saveUninitialized: true,
-  store: MongoDbStore.create({ mongoUrl: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ktdpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0` })
-}));
+app.use(
+  session({
+    secret: 'idkwhatthisdoesngl',
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({
+      mongoUrl: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ktdpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    }),
+  })
+);
 
-
-// database
-await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ktdpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+// database connection
+await mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ktdpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -37,7 +43,7 @@ app.use(express.static(publicPath));
 
 app.get('/dashboard', isAuthenticated, (req, res) => {
   res.sendFile(path.join(publicPath, 'views', 'dashboard.html'));
-})
+});
 
 // auth & snippet routes
 app.use('/', authRoutes);
@@ -45,6 +51,6 @@ app.use('/', snippetRoutes);
 
 export function start() {
   app.listen(port, () => {
-    console.log('Listening at http://localhost')
-  })
+    console.log('Listening at http://localhost');
+  });
 }
